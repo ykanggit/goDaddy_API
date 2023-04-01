31b9f1b541008dd824df39acb85cd9c3d3fe48a1
Yong Kang <yong.kang@iooi.life>
2023-04-01 00:27:36 -0700

    check 3 letter domain

--- is_domain_available.py
+++ is_domain_available.py
@@ -15,42 +15,46 @@ def is_available (domain) -> bool:
         "Authorization": f"sso-key {api_key}:{api_secret}",
         "Content-Type": "application/json"
     }
-    cnt=1
     while True:
-        sys.stdout.write('.')
-        sys.stdout.flush()
-        while cnt:
-            time.sleep(0.1)
-            cnt -= 1
         response = requests.get(
             f"https://api.godaddy.com/v1/domains/available?domain={domain}",
             headers=hdrs
         )
         if response.status_code == 200:
+            sys.stdout.write('.')
+            sys.stdout.flush()
             response_json = response.json()
             if response_json["available"]:
                 logger.info (f"'{domain}' YES")
                 return True
             return False
         else:
-            logger.error (f"Error checking availability for domain '{domain}': {response.text}")
+            sys.stdout.write('x')
+            sys.stdout.flush()
             rj = response.json()
+            logger.error (f"{response.status_code} for '{domain}': {rj}")
             if 'retryAfterSec' in rj:
-                logger.debug (f"API request backoff {rj['retryAfterSec']} second")
-                cnt = rj['retryAfterSec'] * 10
+                sleep (rj['retryAfterSec'])
+            else:
+                return False
+
 
 
 if __name__ == '__main__':
     logger.remove()
     logger.add (sys.stdout, level="DEBUG")
     ok=[]
-    for l1 in range(ord('a'), ord('z') + 1):
-        for l2 in range(ord('a'), ord('z') + 1):
-            domain = chr(l1)+chr(l2)+".ai"
-            if is_available  (domain):
-                ok.append (domain)
+    suffix=['ai','io','com']
+    for s in suffix:
+        for l1 in range(ord('a'), ord('z') + 1):
+            for l2 in range(ord('a'), ord('z') + 1):
+                for l3 in range(ord('a'), ord('z') + 1):
+                    domain = chr(l1)+chr(l2)+chr(l3)+f".{s}"
+                    if is_available  (domain):
+                        ok.append (domain)
     print (ok)
     from slack import MSG, ChatBot
     from cfg import SLACK_WEBHOOK
+    msg = MSG(text=f"{ok}")
     bot = ChatBot(SLACK_WEBHOOK)
-    bot.send (f"{ok}")
+    bot.send (msg)
